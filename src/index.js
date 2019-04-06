f06e70e383726a1f954e0c7221f3da848e06f06e
OscarFabiani <spartan539@yahoo.com>
2019-04-06 13:10:07 -0700

    First working draft.

--- src/index.js
+++ src/index.js
@@ -3,32 +3,79 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Tracker extends React.Component {
+  state = {
+    people: [
+      {
+        name: 'Jess',
+        relationship: 'GF',
+      },
+      {
+        name: 'Hampton',
+        relationship: 'Brother',
+      },
+    ]
+  }
+  handleDelete = (name) => {
+    const people = this.state.people.filter(p => p.name !== name)
+    this.setState ({
+      people: people
+    })
+  }
+  handleSubmit = (name, relationship) => {
+    this.setState ({
+      people : [...this.state.people, {name: name, relationship: relationship}]
+    })
+  }
   render() {
     return (
       <div>
         <h1 className='margin-bottom'>Person Tracker</h1>
         <h2 className={'margin-bottom'}>Add New Person</h2>
-        <AddForm />
+        <AddForm handleSubmit={this.handleSubmit}/>
         <h2 className={'margin-bottom'}>Persons:</h2>
-        <Persons />
+        <Persons people={this.state.people} handleDelete={this.handleDelete}/>
       </div>
     )
   }
 }
 
 class AddForm extends React.Component {
+  state = {
+    name: '',
+    relationship: '',
+  }
+  handleNameChange = (event) => {
+    this.setState ({
+      name: event.target.value
+    })
+  }
+  handleRelationshipChange = (event) => {
+    this.setState ({
+      relationship: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    if (this.state.name && this.state.relationship) {
+      this.props.handleSubmit(this.state.name, this.state.relationship);
+      this.setState ({
+        name: '',
+        relationship: '',
+      })
+    }
+    event.preventDefault();
+  }
   render() {
     return (
-      <form className='margin-bottom'>
+      <form className='margin-bottom' onSubmit={this.handleSubmit}>
         <fieldset>
           <legend>Name</legend>
-          <input type='text'></input>
+          <input type='text' name='name' value={this.state.name} onChange={this.handleNameChange}></input>
         </fieldset>
         <fieldset>
           <legend>Relationship</legend>
-          <input type='text'></input>
+          <input type='text' name='relationship' value={this.state.relationship} onChange={this.handleRelationshipChange}></input>
         </fieldset>
-        <input type='submit'></input>
+        <input type='submit' value='Submit'></input>
       </form>
     )
   }
@@ -36,6 +83,16 @@ class AddForm extends React.Component {
 
 class Persons extends React.Component {
   render() {
+    const people = this.props.people;
+    const peopleRenders = people.map(peep => {
+      return (
+      <Person
+        key={peep.name}
+        name={peep.name}
+        relationship={peep.relationship}
+        handleDelete={this.props.handleDelete}/>
+      )
+    })
     return (
       <table>
         <thead>
@@ -46,18 +103,24 @@ class Persons extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Jess</td>
-            <td>GF</td>
-            <td><button>Delete</button></td>
-          </tr>
-          <tr>
-            <td>Hampton</td>
-            <td>Brother</td>
-            <td><button>Delete</button></td>
-          </tr>
+          {peopleRenders}
         </tbody>
       </table>
+    )
+  }
+}
+
+class Person extends React.Component {
+  handleClick = () => {
+    this.props.handleDelete(this.props.name);
+  }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.relationship}</td>
+        <td><button onClick={this.handleClick}>Delete</button></td>
+      </tr>
     )
   }
 }
@@ -68,3 +131,6 @@ ReactDOM.render(
   <Tracker />,
   document.getElementById('root')
 );
+
+
+//POSSIBLE ADDITIONS: ADD ERRORS FOR EMPTY FIELDS, DONT ALLOW DUPLICATE NAMES
