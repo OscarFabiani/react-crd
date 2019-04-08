f38f1993f63d1e2e52094e112c3cc2c45fa95172
OscarFabiani <spartan539@yahoo.com>
2019-04-08 13:14:42 -0700

    Refactored AddForm component to recieve props from Tracker Component and added state to Tracker component for input values.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 
 class Tracker extends React.Component {
   state = {
+    nameVal: '',
+    relationshipVal: '',
     people: [
       {
         name: 'Jess',
@@ -15,6 +17,17 @@ class Tracker extends React.Component {
       },
     ]
   }
+  updateInput = (event) => {
+    if (event.target.name === 'name') {
+      this.setState ({
+        nameVal: event.target.value
+      })
+    } else {
+      this.setState ({
+        relationshipVal: event.target.value
+      })
+    }
+  }
   handleDelete = (index) => {
     this.setState (prevState => {
       return {people: prevState.people.filter((_, i) => i !== index)}
@@ -22,6 +35,8 @@ class Tracker extends React.Component {
   }
   handleSubmit = (name, relationship) => {
     this.setState ({
+      nameVal: '',
+      relationshipVal: '',
       people : [...this.state.people, {name: name, relationship: relationship}]
     })
   }
@@ -29,13 +44,107 @@ class Tracker extends React.Component {
     return (
       <div>
         <h1 className='margin-bottom'>Person Tracker</h1>
-        <AddForm handleSubmit={this.handleSubmit}/>
+        <AddForm
+          handleSubmit={this.handleSubmit}
+          nameVal={this.state.nameVal}
+          relationshipVal={this.state.relationshipVal}
+          updateInput={this.updateInput}/>
         <h2 className={'margin-bottom'}>Persons:</h2>
         <Persons people={this.state.people} handleDelete={this.handleDelete}/>
       </div>
     )
   }
 }
+
+const AddForm = props => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (props.nameVal && props.relationshipVal) {
+      props.handleSubmit(props.nameVal, props.relationshipVal);
+    }
+  }
+  return (
+    <form className='margin-bottom' onSubmit={handleSubmit}>
+      <fieldset>
+        <legend><h2 className={'margin-bottom'}>Add New Person</h2></legend>
+        <legend>Name</legend>
+        <input type='text' name='name' value={props.nameVal} onChange={props.updateInput}></input>
+        <legend>Relationship</legend>
+        <input className={'margin-bottom'} type='text' name='relationship' value={props.relationshipVal} onChange={props.updateInput}></input>
+        <input className='display-block' type='submit' value='Submit'></input>
+      </fieldset>
+    </form>
+  )
+}
+
+const Persons = props => {
+  const people = props.people;
+  const peopleRenders = people.map((peep, i) => {
+    return (
+    <Person
+      key={peep.name + i}
+      index={i}
+      name={peep.name}
+      relationship={peep.relationship}
+      handleDelete={props.handleDelete}/>
+    )
+  })
+  
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Relationship</th>
+          <th>Remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        {peopleRenders}
+      </tbody>
+    </table>
+  )
+}
+
+const Person = props => {
+  const handleClick = () => {
+    props.handleDelete(props.index);
+  }
+    console.log(props.name + ' just rendered')
+    return (
+      <tr>
+        <td>{props.name}</td>
+        <td>{props.relationship}</td>
+        <td><button onClick={handleClick}>Delete</button></td>
+      </tr>
+    )
+}
+
+
+
+
+ReactDOM.render(
+  <Tracker />,
+  document.getElementById('root')
+);
+
+//CONTINUE COMPARING AND REFINING: I most recently converted the AddForm compopnent from a controlled
+//component using state to an uncontrolled component using refs to access the value of the input
+//fields from the DOM. This seems to necessitate using a constructor to initialize the refs using
+//React.createRef(). I could have (hypothetically) instead removed the fieldset and legend tags and accessed
+//the inputs using event.target[0].value. This would have removed semantic html in favor of a more concise
+//component (no constructor or state). Maybe there is a way to further refine this component. Maybe there is
+//a way for event.target to access child elements...
+
+//After some research I found two other methods for adding refs. One was using an inline function to create the
+//ref thus eliminating the need to use React.createRef() (and thus eliminating the need for a constructor), and
+//the other method was to use a method to set the ref which avoided the inline ref function updating often. I
+//then converted the AddForm component to a functional component and found that the setRef method wasn't working
+//as it did when the component was a class component so I instead reverted to using React.createRef() instead
+//of a setRef method. Maybe a class component would be best in this case. If so, why?
+
+//POSSIBLE ADDITIONS: STOP EACH PERSON COMPONNET FROM RERENDERING, ADD ERRORS FOR EMPTY FIELDS, DONT ALLOW
+//DUPLICATE NAMES
 
 /*
 class AddForm extends React.Component {
@@ -72,6 +181,7 @@ class AddForm extends React.Component {
 }
 */
 
+/*
 const AddForm = props => {
   let input1 = React.createRef();
   let input2 = React.createRef();
@@ -86,87 +196,42 @@ const AddForm = props => {
       relationship.value = '';
     }
   }
-    return (
-      <form className='margin-bottom' onSubmit={handleSubmit}>
-        <fieldset>
-          <legend><h2 className={'margin-bottom'}>Add New Person</h2></legend>
-          <legend>Name</legend>
-          <input type='text' name='name' ref={input1}></input>
-          <legend>Relationship</legend>
-          <input className={'margin-bottom'} type='text' name='relationship' ref={input2}></input>
-          <input className='display-block' type='submit' value='Submit'></input>
-        </fieldset>
-      </form>
-    )
-}
-
-const Persons = props => {
-  const people = props.people;
-  const peopleRenders = people.map((peep, i) => {
-    return (
-    <Person
-      key={peep.name + i}
-      index={i}
-      name={peep.name}
-      relationship={peep.relationship}
-      handleDelete={() => props.handleDelete(i)}/>
-    )
-  })
-  
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Relationship</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {peopleRenders}
-      </tbody>
-    </table>
+    <form className='margin-bottom' onSubmit={handleSubmit}>
+      <fieldset>
+        <legend><h2 className={'margin-bottom'}>Add New Person</h2></legend>
+        <legend>Name</legend>
+        <input type='text' name='name' ref={input1}></input>
+        <legend>Relationship</legend>
+        <input className={'margin-bottom'} type='text' name='relationship' ref={input2}></input>
+        <input className='display-block' type='submit' value='Submit'></input>
+      </fieldset>
+    </form>
   )
 }
 
-const Person = props => {
-  //WHY ISN'T THIS BEING USED YET?
-  const handleClick = () => {
-    this.props.handleDelete(this.props.index);
+const AddForm = props => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target[0].children[2];
+    const relationship = event.target[0].children[4];
+    if (name.value && relationship.value) {
+      props.handleSubmit(name.value, relationship.value);
+      name.value = '';
+      relationship.value = '';
+    }
   }
-    console.log(props.name + ' just rendered')
-    return (
-      <tr>
-        <td>{props.name}</td>
-        <td>{props.relationship}</td>
-        <td><button onClick={props.handleDelete}>Delete</button></td>
-      </tr>
-    )
+  return (
+    <form className='margin-bottom' onSubmit={handleSubmit}>
+      <fieldset>
+        <legend><h2 className={'margin-bottom'}>Add New Person</h2></legend>
+        <legend>Name</legend>
+        <input type='text' name='name'></input>
+        <legend>Relationship</legend>
+        <input className={'margin-bottom'} type='text' name='relationship'></input>
+        <input className='display-block' type='submit' value='Submit'></input>
+      </fieldset>
+    </form>
+  )
 }
-
-
-
-
-ReactDOM.render(
-  <Tracker />,
-  document.getElementById('root')
-);
-
-//CONTINUE COMPARING AND REFINING: I most recently converted the AddForm compopnent from a controlled
-//component using state to an uncontrolled component using refs to access the value of the input
-//fields from the DOM. This seems to necessitate using a constructor to initialize the refs using
-//React.createRef(). I could have (hypothetically) instead removed the fieldset and legend tags and accessed
-//the inputs using event.target[0].value. This would have removed semantic html in favor of a more concise
-//component (no constructor or state). Maybe there is a way to further refine this component. Maybe there is
-//a way for event.target to access child elements...
-
-//After some research I found two other methods for adding refs. One was using an inline function to create the
-//ref thus eliminating the need to use React.createRef() (and thus eliminating the need for a constructor), and
-//the other method was to use a method to set the ref which avoided the inline ref function updating often. I
-//then converted the AddForm component to a functional component and found that the setRef method wasn't working
-//as it did when the component was a class component so I instead reverted to using React.createRef() instead
-//of a setRef method. Maybe a class component would be best in this case. If so, why?
-
-//POSSIBLE ADDITIONS: STOP EACH PERSON COMPONNET FROM RERENDERING, ADD ERRORS FOR EMPTY FIELDS, DONT ALLOW
-//DUPLICATE NAMES
-
+*/
